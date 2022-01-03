70853042d21efea9756b7d481548a3e948a55aad
Olga <olga@gorchichko.com>
2022-01-03 16:28:28 +0300

    Added slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//always write in console npx json-server db.json
 window.addEventListener('DOMContentLoaded', () => {
 
     //Tabs
@@ -194,17 +195,11 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => {
-    //         data.forEach(({img, altimg, title, descr, price}) => {
-    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-    //         });
-    //     });
-
     axios.get('http://localhost:3000/menu')
         .then(data => {
             data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            console.log(data);
             });
         });
 
@@ -287,7 +282,54 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
+    //Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+    
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.innerText = `0${slides.length}`;
+    } else {
+        total.innerText = slides.length;
+    }
+    
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1; 
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+        
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slideIndex < 10) {
+            current.innerText = `0${slideIndex}`;
+        } else {
+            current.innerText = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+          
 });
