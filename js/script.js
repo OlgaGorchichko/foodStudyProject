cd75d6fce5c060ccfed6227279ca92b6b05fb1f1
Olga <olga@gorchichko.com>
2022-01-06 14:04:16 +0300

    Using regular expressions instead of Slice

--- js/script.js
+++ js/script.js
@@ -360,10 +360,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     next.addEventListener('click', () => {  
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == getNumber(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += getNumber(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -380,9 +380,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = getNumber(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= getNumber(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -402,7 +402,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = getNumber(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -422,6 +422,10 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = `0${slideIndex}`;
         }
+    }
+
+    function getNumber(str) {
+        return +str.replace(/\D/g, '');
     }
 
 });
